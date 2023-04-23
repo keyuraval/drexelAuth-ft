5075c50b450db80c3e875913610fe6e7a60e6fdb
Keyur Raval <ravalkeyur054@gmail.com>
2023-04-24 00:06:47 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Signup from "./components/SignUp/index";
 import Login from "./components/Login";
 import HomePage from "./components/cont";
 import Inpt from "./components/quant";
+import Demo from "./components/dmo";
 
 function App() {
   const user = localStorage.getItem("token");
@@ -15,6 +16,8 @@ function App() {
       <Route path="/login" exact element={<Login />} />
       <Route path="/pge" exact element={<HomePage />} />
       <Route path="/inpt" exact element={<Inpt />} />
+      <Route path="/dmo" exact element={<Demo />} />
+
 
 
       <Route path="/" element={<Navigate replace to="/login" />} />
