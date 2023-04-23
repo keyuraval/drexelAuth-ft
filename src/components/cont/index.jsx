8af9d82a72e61ae058bd8ba3555671e62ce6929a
Keyur Raval <ravalkeyur054@gmail.com>
2023-04-23 22:39:18 +0530

    IT

---
+++ src/components/cont/index.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Main from '../Main'
+import './styles.css'
+
+const HomePage = () => {
+    return (
+        <div>
+            <Main />
+            <div className='container'>
+                <p>Welcome to Drexel.</p>
+                <p className='head'>One Stop Cutomer Interaction Solution for Developers.</p>
+            </div>
+            <div class="card text-center mb-3">
+                <div class="card-body">
+                    <h5 class="card-title">Start Free Trial</h5>
+                    <p class="card-text">Get started with free trial.</p>
+                    <a href="/inpt" class="btn btn-primary">Explore</a>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default HomePage
