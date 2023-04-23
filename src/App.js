410a3f2619cd40af0858389753dffb0da517e590
Keyur Raval <ravalkeyur054@gmail.com>
2023-04-23 22:41:01 +0530

    IT

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { Route, Routes, Navigate } from "react-router-dom";
 import Main from "./components/Main";
 import Signup from "./components/SignUp/index";
 import Login from "./components/Login";
+import HomePage from "./components/cont";
+import Inpt from "./components/quant";
 
 function App() {
   const user = localStorage.getItem("token");
@@ -11,6 +13,10 @@ function App() {
       {user && <Route path="/" exact element={<Main />} />}
       <Route path="/signup" exact element={<Signup />} />
       <Route path="/login" exact element={<Login />} />
+      <Route path="/pge" exact element={<HomePage />} />
+      <Route path="/inpt" exact element={<Inpt />} />
+
+
       <Route path="/" element={<Navigate replace to="/login" />} />
     </Routes>
   );
