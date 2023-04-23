94e5a9beb02c97e817d948d5036b0d02c2ba4230
Keyur Raval <ravalkeyur054@gmail.com>
2023-04-24 00:53:58 +0530

    UI changes

--- src/components/quant/index.jsx
+++ src/components/quant/index.jsx
@@ -5,9 +5,9 @@ const Inpt = () => {
     return (
         <div>
             <Main />
-            <br /><br /><br /><br />
+            <br /><br />
             <div class="input-group mb-3 container">
-                <label class="input-group-text" for="inputGroupSelect01">Options</label>
+                <label class="input-group-text" for="inputGroupSelect01">Choose Category</label>
                 <select class="form-select" id="inputGroupSelect01">
                     <option selected>Choose Catagory</option>
                     <option value="1">Basic Interaction</option>
